ca48e2dd121fb304f568fe77ddf39bf17a06782a
aaksi <yyff91@mail.ru>
2021-02-04 01:47:51 +0300

    added header, top, advantages, services, price , reviews

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,21 @@
 
 $(function(){
    
+    $('.advantages__btn').on('click', function(){
+        $('.advantages__descr').fadeIn();
+    });
+    $('.services__btn').on('click', function(){
+        $('.services__card:nth-child(n+4)').fadeIn();
+    });
+
+    
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slider__btn slider__btn--left"></button>',
+        nextArrow:'<button class="slider__btn slider__btn--right"></button>',
+        dots: true,
+    });
 
 });
